4c3c9104509a01f459dd1364204bed80fc1084ce
Cristian Pinzón <faykris28@gmail.com>
2021-06-29 14:23:03 -0500

    update console.py - erase parse function

--- console.py
+++ console.py
@@ -114,10 +114,5 @@ class HBNBCommand(cmd.Cmd):
             self.file = None
 
 
-def parse(arg):
-    'Convert a series of zero or more numbers to an argument tuple'
-    return tuple(map(int, arg.split()))
-
-
 if __name__ == '__main__':
     HBNBCommand().cmdloop()

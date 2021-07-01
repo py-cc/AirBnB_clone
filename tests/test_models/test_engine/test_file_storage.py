ce87c05fb9b656b1544874ea4ae27964e600ab18
Cristian Pinzón <faykris28@gmail.com>
2021-06-30 22:32:05 -0500

    update test files - add test_file_storage.py, test_base_model.py

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import unittest
+
+
+class TestFileStorage(unittest.TestCase):
+    """ TestFileStorage unittest"""
+
+    def test_storage(self):
+        self.assertTrue(True)
